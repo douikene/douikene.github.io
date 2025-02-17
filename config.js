0b96a2f4779633add658e88e31c36ef08b9ff88f
douikene <d.ouikene@gmail.com>
2025-02-17 12:06:30 +0800

    chore: remove backend compenemt

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    email: 'd.ouikene@gmail.com',
-    phone: '+65 8147 8720',
-    location: 'Singapore',
-    github: 'https://github.com/douikene',
-    linkedin: 'https://www.linkedin.com/in/douikene/',
-}; 

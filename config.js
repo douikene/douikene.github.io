67f0dec0965043ebd5178077c40b0a376e291a7d
douikene <d.ouikene@gmail.com>
2025-02-17 11:57:49 +0800

    chore: update GitHub Actions workflow and remove test files
    - Downgrade actions/checkout to v2
    - Add secret email and phone output in GitHub Actions
    - Remove JavaScript test files
    - Update CV data configuration
    - Minor language level adjustment in Arabic translation

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    email: 'd.ouikene@gmail.com',
+    phone: '+65 8147 8720',
+    location: 'Singapore',
+    github: 'https://github.com/douikene',
+    linkedin: 'https://www.linkedin.com/in/douikene/',
+}; 
